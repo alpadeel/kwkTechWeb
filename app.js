935d74a3d80e59b06d038128cf94d90e1c1f2113
shakeel-lynxera <shakeelafridi@lynxera.com>
2021-06-22 18:40:42 +0500

    zoho

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 var nodemailer = require('nodemailer');
+const path = require("path")
 require("dotenv").config();
-
 // body parser middleware
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded( { extended: false } )); // this is to handle URL encoded data
 app.set('views', './views');
 app.set('view engine', 'ejs');
+
 
 // use res.render to load up an ejs view file
 
@@ -18,23 +20,38 @@ app.get('/', function(req, res) {
 });
 app.post("/ajax/email", function(request, response) {
     // create reusable transporter object using the default SMTP transport
-      const transporter = nodemailer.createTransport({
-          host: "smtp.gmail.com",
-          port: 465,
+    
+    const transporter = nodemailer.createTransport({
+    host: "smtppro.zoho.com",
+     port: 465,
           secure: true,
           auth: {
-             // this should be YOUR GMAIL account
               user: process.env.EMAIL,
               pass: process.env.PASSWORD
           }
       });
   
-      var textBody = `FROM: ${request.body.name} EMAIL: ${request.body.email} MESSAGE: ${request.body.message}`;
-      var htmlBody = `<h2>Mail From Contact Form</h2><p>from: ${request.body.name} <a href="mailto:${request.body.email}">${request.body.email}</a></p><p>${request.body.message}</p>`;
+      var textBody = 
+      `FROM: ${request.body.name}
+       EMAIL: ${request.body.email}
+       MESSAGE: ${request.body.message}
+       COMPANY_NAME: ${request.body.cname}
+       PHONE_NUMBER: ${request.body.pnumber}
+       DESCRIOPTION: ${request.body.description}
+       `
+      var htmlBody = 
+      `<h2> Kwk Tech </h2>
+      <p> </br>
+      from: ${request.body.name}</br>
+      Company Name:  ${request.body.companyName}</br>
+      Phone Number:  ${request.body.phoneNumber}</br>
+      email: <a href="mailto:${request.body.email}">${request.body.email}</a>
+      </p>
+      <p>${request.body.Description}</p>`;
       var mail = {
-          from: "your_account@gmail.com", // sender address
-          to: "your_account@gmail.com", // list of receivers (THIS COULD BE A DIFFERENT ADDRESS or ADDRESSES SEPARATED BY COMMAS)
-          subject: "Mail From Contact Form", // Subject line
+          from: process.env.EMAIL, // sender address
+          to:  process.env.EMAIL, // list of receivers (THIS COULD BE A DIFFERENT ADDRESS or ADDRESSES SEPARATED BY COMMAS)
+          subject: "Contact Form", // Subject line
           text: textBody,
           html: htmlBody
       };
@@ -46,8 +63,8 @@ app.post("/ajax/email", function(request, response) {
               response.json({ message: "message not sent: an error occured; check the server's console log" });
           }
           else {
-              response.json({ message: `message sent: ${info.messageId}` });
-          }
+              response.json({ message: `message sent` });
+            }
       });
   });
 
